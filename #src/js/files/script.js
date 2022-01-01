e3b6c0bacc1a6af69c3b8523228abf3a565736c0
Nikita Tsykunov <nikitaodessafreelance@gmail.com>
2022-01-01 18:52:45 +0200

    first commit

---
+++ #src/js/files/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+   // header
+   const header = document.querySelector('.header');
+
+   const callback = function (entries, observer) {
+      if (entries[0].isIntersecting) {
+         header.classList.remove('_scroll');
+      } else {
+         header.classList.add('_scroll');
+      }
+   };
+
+   const headerObserver = new IntersectionObserver(callback);
+   headerObserver.observe(header);
+
+   
+}); // end
